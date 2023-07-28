034499a872324725e6727fd7ee5cd5daba2ecf3f
28frozen <zzahrafatim4@gmail.com>
2023-07-27 19:27:16 -0700

    0x12.c-singlylinkedlists

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,19 +1,7 @@
-section .data
-    msg db "Hello, Holberton!", 10
-    len equ $ - msg
+#include <stdio.h>
 
-section .text
-    global _start
-
-_start:
-    ; Write the string to the standard output (stdout)
-    mov rax, 1            ; syscall number for sys_write
-    mov rdi, 1            ; file descriptor 1 (stdout)
-    mov rsi, msg          ; pointer to the message
-    mov rdx, len          ; length of the message
-    syscall
-
-    ; Exit the program
-    mov rax, 60           ; syscall number for sys_exit
-    xor rdi, rdi          ; exit status 0
-    syscall
+int main(void)
+{
+    printf("Hello, Holberton\n");
+    return 0;
+}
