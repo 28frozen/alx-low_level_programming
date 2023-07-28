585b0468a6619d7a5129fee0a24a8af8c26392ee
28frozen <zzahrafatim4@gmail.com>
2023-07-27 19:59:41 -0700

    0x12.c-singlylinkedlists

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,20 +1,13 @@
 section .data
-    hello db "Hello, Holberton", 0
-    format db "%s", 10, 0
+	format db 'Hello, Holberton', 0
 
 section .text
-    extern printf
-    global main
+	global main
+	extern printf
 
 main:
-    ; Print "Hello, Holberton" with a new line
-    push rbp
-    mov rdi, format
-    mov rsi, hello
-    call printf
-    pop rbp
-
-    ; Exit the program
-    xor rdi, rdi          ; exit status 0
-    mov rax, 60           ; syscall number for sys_exit
-    syscall
+	mov edi, format
+	xor eax, eax
+	call printf
+	mov eax, 0
+	ret
