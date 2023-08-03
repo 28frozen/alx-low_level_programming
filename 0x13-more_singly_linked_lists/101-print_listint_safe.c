8bc436f5cc0668f559b2d4d4ddd0064d1be6855d
28frozen <zzahrafatim4@gmail.com>
2023-08-02 18:47:12 -0700

    0x13.CMoresinglylinkedlists

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -2,50 +2,6 @@
 #include <stdio.h>
 
 /**
- * count_nodes - Counts the number of nodes in a linked list
- * @head: A pointer to the head of the listint_t list
- *
- * Return: The number of nodes in the list.
- */
-size_t count_nodes(const listint_t *head)
-{
-	size_t count = 0;
-
-	while (head)
-	{
-		count++;
-		head = head->next;
-	}
-
-	return (count);
-}
-
-/**
- * is_infinite - Checks if a linked list is infinite (looped)
- * @head: A pointer to the head of the listint_t list
- *
- * Return: 1 if the list is infinite, 0 otherwise.
- */
-int is_infinite(const listint_t *head)
-{
-	const listint_t *tortoise, *hare;
-
-	tortoise = head;
-	hare = head;
-
-	while (tortoise && hare && hare->next)
-	{
-		tortoise = tortoise->next;
-		hare = hare->next->next;
-
-		if (tortoise == hare)
-			return (1);
-	}
-
-	return (0);
-}
-
-/**
  * print_listint_safe - Prints a listint_t list safely.
  * @head: A pointer to the head of the listint_t list.
  *
@@ -53,21 +9,29 @@ int is_infinite(const listint_t *head)
  */
 size_t print_listint_safe(const listint_t *head)
 {
-	size_t count = 0;
+	size_t nodes = 0;
+	const listint_t *current = head;
+	const listint_t *check;
+	size_t i;
 
-	if (is_infinite(head))
+	while (current)
 	{
-		printf("-> [%p] %d\n", (void *)head, head->n);
-		return (1);
+		printf("[%p] %d\n", (void *)current, current->n);
+		nodes++;
+
+		check = head;
+		for (i = 0; i < nodes; i++)
+		{
+			if (check == current->next)
+			{
+				printf("-> [%p] %d\n", (void *)current->next, current->next->n);
+				return (nodes);
+			}
+			check = check->next;
+		}
+
+		current = current->next;
 	}
 
-	count = count_nodes(head);
-	while (head)
-	{
-		printf("[%p] %d\n", (void *)head, head->n);
-		head = head->next;
-		count--;
-	}
-
-	return (count);
+	return (nodes);
 }
