63cdc4ea01872639444a398716333359ddc29745
28frozen <zzahrafatim4@gmail.com>
2023-11-03 18:42:29 -0700

    0x15. C - File I/O

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,46 +1,51 @@
 #include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
 #include <fcntl.h>
 #include <unistd.h>
 
 /**
- * read_textfile - Reads a text file and prints it to the POSIX standard output.
- * @filename: The name of the file to read.
- * @letters: The number of letters to read and print.
+ * read_textfile - Reads a text file and prints it to the POSIX standard output
+ * @filename: Name of the file to read
+ * @letters: Number of letters to read and print
  *
- * Return: The actual number of letters it could read and print.
+ * Return: The actual number of letters it could read and print
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
-	ssize_t bytes_read, bytes_written;
+	ssize_t read_bytes, write_bytes;
 	char *buffer;
 
-	if (filename == NULL)
-		return (0);
-
-	buffer = malloc(letters);
-	if (buffer == NULL)
+	if (!filename)
 		return (0);
 
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
-	{
-		free(buffer);
 		return (0);
-	}
 
-	bytes_read = read(fd, buffer, letters);
-	if (bytes_read == -1)
+	buffer = malloc(sizeof(char) * letters);
+	if (!buffer)
 	{
-		free(buffer);
 		close(fd);
 		return (0);
 	}
 
-	bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
-
-	free(buffer);
+	read_bytes = read(fd, buffer, letters);
 	close(fd);
 
-	return (bytes_written);
+	if (read_bytes == -1)
+	{
+		free(buffer);
+		return (0);
+	}
+
+	write_bytes = write(STDOUT_FILENO, buffer, read_bytes);
+
+	free(buffer);
+
+	if (read_bytes != write_bytes)
+		return (0);
+
+	return (write_bytes);
 }
